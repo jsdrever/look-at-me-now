b68a7b744463db401a54ac513b299d491caa6261
jsdrever <jsdrever09@gmail.com>
2022-12-22 17:39:18 -0600

    stench of desperation

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # look-at-me-now
 #### Criterion
 Creates a password when "generate password" is clicked. Uses prompts to select if it has uppercase, lowercase, symbols, and length. After these criterion are selected a password is displayed in an alert.
+
+###
+dont forget to put the deployed URL here again and fix the wording of your copy pasted readme from the class github
--- script.js
+++ script.js
@@ -1,5 +1,90 @@
 // Assignment Code
+
+
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
 var generateBtn = document.querySelector("#generate");
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbols: getRandomSymbols
+  };
+
+function generatePassword() {
+    
+    let generatedPassword ="finalPassword";
+
+    const typesCount = getRandomLower + getRandomUpper + getRandomNumber + getRandomSymbols;
+  
+    const typesArr = [{ getRandomLower }, { getRandomUpper }, { getRandomNumber }, { getRandomSymbols }].filter 
+    (
+      item => Object.values(item)[0]
+    );
+  
+    if(typesCount === 0) {
+      return '';
+    }
+    for(let i = 0; i < length; i += typesCount) {
+      typesArr.forEach(type => {
+        const funcName = Object.keys(type)[0];
+  
+        generatePassword += randomFunc[funcName]();
+      });
+    }
+  
+    const finalPassword = generatedPassword.slice(0, length);
+    return finalPassword;
+  } 
+//it needs to prompt an alert
+//need to have all the uppercase lowercase symbols and numbers and how many characters its length will be
+//Show the new password. I think i want it to show in the prompt prob 
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  }
+  function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  }
+  function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  }
+  function getRandomSymbols() {
+    const symbols = "!@#$%^&*()[]{}=+></,.";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+  }
+
+  var chosenCharactersArr = []
+  var passwordstring = ''
+
+if (confirm('Include capital letters?')) {
+    chosenCharactersArr.push(getRandomUpper)
+  }
+
+  if (confirm('Include lowercase letters?')) {
+    chosenCharactersArr.push(getRandomLower)
+  }
+  
+  if (confirm('Include numbers?')) {
+    chosenCharactersArr.push(getRandomNumber)
+  }
+ 
+  if (confirm('Include symbols?')) {
+    chosenCharactersArr.push(getRandomSymbols)
+  };
+// for (var i= 0; i < password.results- i want to loop through all the results and put them together; i++){
+    //console.log('results + i)
+//}
+console.log(getRandomUpper());
+console.log(getRandomLower());
+console.log(getRandomNumber());
+console.log(getRandomSymbols());
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +97,23 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+  
+    resultEl.innerText = generatePassword(
+      getRandomLower, 
+      getRandomUpper, 
+      getRandomNumber, 
+      getRandomSymbols
+      );
+  
+
+//PROMPT-PROMPT example for help
+//let person = prompt("are you ready", "YES!!");
+
+
+    alert("display the password now");
+
+
+//does it need to be an append with api selector stuff document.elements.results appendResults(something);
+
+
